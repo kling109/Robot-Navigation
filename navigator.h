7d86929910ec2037f4fab5c5eb1f16fbe96a8563
Trevor Kling <kling109@mail.chapman.edu>
2020-02-18 12:45:32 -0800

    Added navigator header

---
+++ navigator.h
@@ -0,0 +1,28 @@
+/*******************************************************************************
+Robot Navigator
+
+A simple robot navigation program.  The program takes an input of a gameboard,
+represented by a text file, and a specification of a type of search algorithm.
+The program then determines the best path for the robot to take based on the
+chosen search paradigm.
+
+Author: Trevor Kling
+Email: kling109@mail.chapman.edu
+Course: CPSC 390 Artificial Intelligence
+Student ID: 002270716
+Last Date Modified: 02/18/2020
+*******************************************************************************/
+
+/*
+Navigation class for robot decision-making.  This program makes accesses to the
+world class to determine how to move about the game board.  The program takes
+an input of a world object and a navigation method, then traverses the world
+based on the given criteria.
+*/
+
+class Navigation {
+  public:
+
+  private:
+
+}
